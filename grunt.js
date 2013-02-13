7e43946a9dedb8bcab4d25a2a27cdafea4c9affd
Emmanuel Pelletier <pelletier.emmanuel@gmail.com>
2013-02-13 20:07:33 +0100

    ajout de grunt pr concat/min les fichiers js/css

---
+++ grunt.js
@@ -0,0 +1,26 @@
+module.exports = function(grunt) {
+	grunt.loadNpmTasks('grunt-contrib-mincss');
+
+	grunt.initConfig({
+		concat: {
+			dist: {
+				src: [ 'js/microajax.js', 'js/Toast.js', 'js/geolocation.js', 'leaflet/leaflet.js', 'js/Leaflet.Control.Geolocation.js', 'js/script.js'],
+				dest: 'dist/scripts.js'
+			}
+		},
+		min: {
+			dist: {
+				src: ['dist/scripts.js'],
+				dest: 'dist/scripts.min.js'
+			}
+		},
+		mincss: {
+			dist: {
+				files: {
+					'dist/styles.min.css': ['leaflet/leaflet.css', 'css/style.css']
+				}
+			}
+		}
+	});
+	grunt.registerTask('default', 'concat min mincss');
+};
--- js/script.js
+++ js/script.js
@@ -2,9 +2,6 @@
  * A L ARAAAAAAAACHEEUUUH
  */
 (function() {
-	var microAjax = function(url,callbackFunction){this.bindFunction=function(caller,object){return function(){return caller.apply(object,[object])}};this.stateChange=function(object){if(this.request.readyState==4)this.callbackFunction(this.request)};this.getRequest=function(){if(window.ActiveXObject)return new ActiveXObject("Microsoft.XMLHTTP");else if(window.XMLHttpRequest)return new XMLHttpRequest;return false};this.postBody=arguments[2]||"";this.callbackFunction=callbackFunction;this.url=url;
-	this.request=this.getRequest();if(this.request){var req=this.request;req.onreadystatechange=this.bindFunction(this.stateChange,this);if(this.postBody!==""){req.open("POST",url,true);req.setRequestHeader("X-Requested-With","XMLHttpRequest");req.setRequestHeader("Content-type","application/x-www-form-urlencoded");req.setRequestHeader("Connection","close")}else req.open("GET",url,true);req.send(this.postBody)}};
-
 	var layer,
 		map,
 		angers = new L.LatLngBounds(new L.LatLng(47.4, -0.68), new L.LatLng(47.53, -0.43)),
