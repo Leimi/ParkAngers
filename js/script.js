4b8fff6bdbeae1392e8b12249763c57a18545d25
Emmanuel Pelletier <pelletier.emmanuel@gmail.com>
2013-02-13 14:13:36 +0100

    Récupération des places de parking libre depuis sara-angers

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+/**
+ * A L ARAAAAAAAACHEEUUUH
+ */
 (function() {
 	var layer,
 		map,
@@ -5,7 +8,23 @@
 		angers = new L.LatLngBounds(new L.LatLng(47.4, -0.68), new L.LatLng(47.53, -0.43)),
 		zoomControl,
 		geolocationControl,
-		toast = new Toast();
+		toast = new Toast(),
+		parkingsCoords = {
+			// "Parking Ralliement": ,
+			// "Parking République": ,
+			// "Parking Saint Laud": ,
+			// "Parking Marengo": ,
+			// "Parking Haras": ,
+			// "Parking Du Mail": ,
+			// "Parking Leclerc": ,
+			// "Parking Molière": ,
+			// "Parking Mitterrand": ,
+			// "Parking Bressigny": ,
+			// "Parking Poissonnerie": ,
+			// "Parking CHU": ,
+			// "Parking Berges de Maine":
+		},
+		parkingsPlaces = {};
 
 	layer = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 		attribution: '&copy; OpenStreetMap, CC-BY-SA',
@@ -25,5 +44,23 @@
 	geolocationControl = new L.Control.Geolocation({position: 'topleft', bounds: angers }).addTo(map);
 	geolocationControl.on('message', function(message) {
 		toast.show(message.text, message.state);
+	});
+
+	getParkings = function(data) {
+		var links = data.query.results.a || null,
+			name = null,
+			count = null;
+		if (!links)
+			return false;
+		$.each(links, function(i, val) {
+			name = $.trim(val.content) || null;
+			count = val.small.content || null;
+			if (name && count)
+				parkingsPlaces[name] = count*1;
+		});
+	};
+
+	$.ajax({ url: "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%3D%22http%3A%2F%2Fwww.sara-angers.fr%2Fmobile%22%20and%20xpath%3D'%2F%2Fli%2Fa%5Bcontains(%40href%2C%22%23%22)%5D'&format=json&callback=getParkings",
+		dataType: 'jsonp'
 	});
 })();
